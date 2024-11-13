4b440dd4ca664cba0bb5c089a58f420d0a46ee6c
mazama923 <brasses_wearier0a@icloud.com>
2024-11-13 20:50:43 +0100

    Add recipes for Lazy

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,6 @@
 return {
-  -- Installer le thème rose-pine
   { "rose-pine/neovim", name = "rose-pine" },
 
-  -- Configurer LazyVim pour utiliser rose-pine comme colorschema
   {
     "LazyVim/LazyVim",
     opts = {
