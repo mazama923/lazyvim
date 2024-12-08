15ab86e4b85a410c5a85ff212acf89ac1931309b
mazama923 <brasses_wearier0a@icloud.com>
2024-12-08 23:19:54 +0100

    Add ansible vault with commande

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.ansible_vault_password_file = "~/.vault_password"
+vim.g.ansible_vault_no_unquote = 1
