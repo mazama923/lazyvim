7c012586643dbf54b2a1b9bff9465ac82eabf967
Benjamin <42283374+mazama923@users.noreply.github.com>
2025-01-09 23:53:16 +0100

    Update autocmds.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,13 +8,3 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
   command = "silent! wall",
   nested = true,
 })
-
--- Disable SSL verification for curl with Treesitter to bypass company proxy issues
-vim.api.nvim_create_autocmd("User", {
-  pattern = "nvim-treesitter",
-  callback = function()
-    require("nvim-treesitter.install").command_extra_args = {
-      curl = { "--insecure" },
-    }
-  end,
-}
