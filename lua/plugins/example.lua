814eeaf7d5f01bde7d6de5e1d6a6319699fdc2f2
Benjamin <42283374+mazama923@users.noreply.github.com>
2025-01-09 23:46:22 +0100

    Update example.lua

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -139,10 +139,6 @@ return {
         "tsx",
         "typescript",
       })
-      -- Disable ssl only for curl with Treesitter because it causes problems with the company's proxies
-      require("nvim-treesitter.install").command_extra_args = {
-        curl = { "--insecure" },
-      }
     end,
   },
 
