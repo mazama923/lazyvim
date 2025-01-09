a8d9d661117f317f59928e18e3f10bae84723c84
Benjamin <42283374+mazama923@users.noreply.github.com>
2025-01-09 23:48:44 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- Disable ssl only for curl with Treesitter because it causes problems with the company's proxies
-require("nvim-treesitter.install").command_extra_args = {
-  curl = { "--insecure" },
-}
+-- Disable SSL verification for curl with Treesitter to bypass company proxy issues
+vim.api.nvim_create_autocmd("User", {
+  pattern = "nvim-treesitter",
+  callback = function()
+    require("nvim-treesitter.install").command_extra_args = {
+      curl = { "--insecure" },
+    }
+  end,
+})
