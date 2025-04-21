7fc11aa54b6106c8237782252e41bfa836dbe1aa
mazama923 <brasses_wearier0a@icloud.com>
2025-04-21 23:33:44 +0200

    Remove vault file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.ansible_vault_password_file = "~/.vault_password"
+-- vim.g.ansible_vault_password_file = "~/.vault_password"
 vim.g.ansible_vault_no_unquote = 1
 vim.o.background = "light" -- or "dark"
 -- Disable mouse
