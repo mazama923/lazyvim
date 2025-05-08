64cdf56db1f570275a31b8b71574ae38997bbc75
mazama923 <brasses_wearier0a@icloud.com>
2025-05-08 13:15:36 +0200

    add update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,21 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- vim.g.ansible_vault_password_file = "~/.vault_password"
-vim.g.ansible_vault_no_unquote = 1
-vim.o.background = "light" -- or "dark"
+
 -- Disable mouse
 vim.opt.mouse = ""
 -- Copy & paste with mouse
 vim.g.clipboard = {
-  name = 'myClipboard',
+  name = "myClipboard",
   copy = {
-    ['+'] = 'xclip -selection clipboard',
-    ['*'] = 'xclip -selection clipboard',
+    ["+"] = "xclip -selection clipboard",
+    ["*"] = "xclip -selection clipboard",
   },
   paste = {
-    ['+'] = 'xclip -selection clipboard -o',
-    ['*'] = 'xclip -selection clipboard -o',
+    ["+"] = "xclip -selection clipboard -o",
+    ["*"] = "xclip -selection clipboard -o",
   },
 }
 -- Add system clipboard
