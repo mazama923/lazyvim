45cee2314b935db68f2b841ffb34ead5fc136d1c
mazama923 <brasses_wearier0a@icloud.com>
2025-06-07 15:26:39 +0200

    add config for neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules",
+          },
+          never_show = {
+            ".git",
+          },
+        },
+      },
+    },
+  },
+}
