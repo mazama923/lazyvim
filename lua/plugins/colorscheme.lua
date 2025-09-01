40f6c3f1fef774163fd8bbfde094c902ff0ead2f
mazama923 <brasses_wearier0a@icloud.com>
2025-09-01 23:14:57 +0200

    FIX error catppucin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,4 +7,15 @@ return {
       colorscheme = "catppuccin-latte",
     },
   },
+
+  -- FIX ERROR: attempt to call field 'get'
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
 }
