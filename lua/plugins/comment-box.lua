de71e155d5872a4c67cab8e59fd5b0ecd9cea4a7
mazama923 <42283374+mazama923@users.noreply.github.com>
2026-01-05 19:14:09 +0100

    add comment-box

---
+++ lua/plugins/comment-box.lua
@@ -0,0 +1,11 @@
+return {
+  "LudoPinelli/comment-box.nvim",
+  keys = {
+    { "<Leader>cb", "<Cmd>CBccbox<CR>", mode = { "n", "v" }, desc = "Title box" },
+    { "<Leader>ct", "<Cmd>CBllline<CR>", mode = { "n", "v" }, desc = "Named line" },
+    { "<Leader>cl", "<Cmd>CBline<CR>", mode = "n", desc = "Single line" },
+    { "<Leader>cm", "<Cmd>CBllbox14<CR>", mode = { "n", "v" }, desc = "Marked box" },
+    { "<Leader>cd", "<Cmd>CBd<CR>", mode = { "n", "v" }, desc = "Delete box" },
+  },
+}
+
